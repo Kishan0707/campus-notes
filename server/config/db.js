f0b59e7cbe16aa166e9facffa17c1a85071d846e
Kishan Ahir <kk57161@gmail.com>
2026-02-16 17:58:38 +0530

    Fix MySQL connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2";
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mysql@123",
-  database: "campus_note",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
+
 db.connect((err) => {
   if (err) {
     console.log("====================================");
@@ -16,4 +19,5 @@ db.connect((err) => {
     console.log("====================================");
   }
 });
+
 export default db;
