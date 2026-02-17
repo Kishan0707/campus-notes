02c7ab1578446b7b7933003b9741f9bdda6f743c
Kishan Ahir <kk57161@gmail.com>
2026-02-17 14:59:23 +0530

    connected DB

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
